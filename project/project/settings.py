d839b00732f4d29549abc1e542cb154269c4e419
Oskar Sharipov <oskar.sharipov[at]tuta.io>
2020-06-08 19:21:16 +0300

    Collect statics

--- project/project/settings.py
+++ project/project/settings.py
@@ -95,3 +95,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
